9462068d63d150700117a6cb8e6819c1b5393164
RiRyCasals <rirycasals@gmail.com>
2022-02-04 10:31:06 +0900

    2,3層目を実装

--- src/DLFrom0/matrix.nim
+++ src/DLFrom0/matrix.nim
@@ -47,14 +47,12 @@ proc relu[M, N, T](matrix: Matrix2d[M, N, T]): Matrix2d[M, N, T] =
   return matrixB
 
 # 転置行列：実装してみたかったからやった
-#[
 proc transpose[I, J, T](matrix: Matrix2d[I, J, T]): Matrix2d[J, I, T] =
   var matrixT: Matrix2d[J, I, T]
   for j in 0..<J:
     for i in 0..<I:
       matrixT[j][i] = matrix[i][j]
   return matrixT
-]#
 
 
 when isMainModule:
@@ -82,17 +80,58 @@ when isMainModule:
   let matrixX: Matrix2d[i, j, float] = [
     [1.0, 0.5]
   ]
-  let matrixW: Matrix2d[j, k, float] = [
+  ## 1層
+  let matrixW1: Matrix2d[j, k, float] = [
     [0.1, 0.3, 0.5],
     [0.2, 0.4, 0.6],
   ]
-  let matrixB: Matrix2d[i, k, float] = [
+  let matrixB1: Matrix2d[i, k, float] = [
     [0.1, 0.2, 0.3]
   ]
   
-  echo matrixX.shape
-  echo matrixW.shape
-  echo matrixB.shape
-  echo matrixX.dot(matrixW)
-  echo matrixX.dot(matrixW).add(matrixB)
-  echo matrixX.dot(matrixW).add(matrixB).sigmoid()
+  echo "shape x: ", matrixX.shape
+  echo "shape w1: ", matrixW1.shape
+  echo "shape b1: ", matrixB1.shape
+
+  let matrixA1 = matrixX.dot(matrixW1).add(matrixB1)
+  echo "shape a1: ", matrixA1.shape
+  echo "a1: ", matrixA1
+
+  let matrixZ1 = sigmoid(matrixA1)
+  echo "z1: ", matrixZ1
+
+  ## 2層
+  let matrixW2: Matrix2d[k, j, float] = [
+    [0.1, 0.4],
+    [0.2, 0.5],
+    [0.3, 0.6],
+  ]
+  let matrixB2: Matrix2d[i, j, float] = [
+    [0.1, 0.2]
+  ]
+
+  echo "shape w2: ", matrixW2.shape
+  echo "shape b2: ", matrixB2.shape
+
+  let matrixA2 = matrixZ1.dot(matrixW2).add(matrixB2)
+  echo "shape a2: ", matrixA2.shape
+  echo "a2: ", matrixA2
+
+  let matrixZ2 = sigmoid(matrixA2)
+  echo "z2: ", matrixZ2
+
+  ## 3層
+  let matrixW3: Matrix2d[j, j, float] = [
+    [0.1, 0.3],
+    [0.2, 0.4],
+  ]
+  let matrixB3: Matrix2d[i, j, float] = [
+    [0.1, 0.2]
+  ]
+
+  echo "shape w3: ", matrixW3.shape
+  echo "shape b3: ", matrixB3.shape
+
+  let matrixA3 = matrixZ2.dot(matrixW3).add(matrixB3)
+  echo "shape a3: ", matrixA3.shape
+  echo "a3: ", matrixA3
