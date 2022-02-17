6258f21bf6e25de95412ceb998a6d1379af588d3
RiRyCasals <rirycasals@gmail.com>
2022-02-17 23:33:15 +0900

    コメントを追加

--- src/exercise/trial/trialTemplate.nim
+++ src/exercise/trial/trialTemplate.nim
@@ -1,3 +1,14 @@
+#[
+  templateは抽象構文木を置換する仕組み
+  templateの引数には通常の型に加えて, meta types (untyped, typed, typedesc) が使える
+    untyped:  template引数の型解決をtemplate呼び出し時に行わないようにさせる
+    typed:    template引数の型解決をtemplate呼び出し時に行うようにさせる
+    typedesc: 型名をtemplate引数にさせる
+  template呼び出し時に，呼び出し式の最後に : と続けてブロック文を記述するとtemplate引数の最後にブロック文を渡せる
+]#
+
+# どういうときに使えばいいんだろう？？？
+
 template echoBlock(blockName, x: untyped): untyped =
   echo "===== ", astToStr(blockName), " ====="
   echo astToStr(x)
