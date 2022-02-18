ab4d6272ea2c35afb6cfda341d617f337bc611e0
RiRyCasals <rirycasals@gmail.com>
2022-02-18 23:26:09 +0900

    jpgファイル操作系の開発開始

--- src/exercise/trial/trialFiles.nim
+++ src/exercise/trial/trialFiles.nim
@@ -1,6 +1,7 @@
 import os
 
-let filePath = "trial/testFile.txt"
+echo "======= text file ======="
+var filePath = "trial/testFile.txt"
 
 if fileExists(filePath):
   for line in lines(filePath):
@@ -8,3 +9,30 @@ if fileExists(filePath):
 else:
   echo filePath, " is not found."
   echo "Try execute in the exercise directory."
+
+
+echo "======= jpg file ======="
+filePath = "trial/nimPython.jpg"
+
+if fileExists(filePath):
+  let file = readFile(filePath)
+  echo file.type
+  #echo file[0..31] #-> binaryを文字列にしたものが入ってる
+else:
+  echo filePath, " is not found."
+  echo "Try execute in the exercise directory."
+
+
+echo "======= jpg file 2 ======="
+#proc loadImage(path: string): openArray[int] =
+proc loadImage(path: string) =
+  let file: File = open(path, fmRead)
+  echo file.type
+  echo file.getFileSize
+  echo file.getFilePos
+  setFilePos(file, 10)
+  echo file.getFilePos
+  file.close()
+
+if fileExists(filePath):
+  loadImage(filePath)
