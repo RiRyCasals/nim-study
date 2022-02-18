53d576d18e9d82f15a53f84b02b31a128fd30094
RiRyCasals <rirycasals@gmail.com>
2022-02-18 09:53:46 +0900

    乱数期の初期化，抽出，シャッフル

--- src/exercise/trial/trialRandom.nim
+++ src/exercise/trial/trialRandom.nim
@@ -1,6 +1,6 @@
 import
-  random,
-  mersenne # use `std/random` instead; mersenne is deprecated って言われた
+  random
+  # mersenne # use `std/random` instead; mersenne is deprecated って言われた
 
 
 # random
@@ -19,6 +19,7 @@ import
 ]#
 
 ## デフォルトの乱数器は安全でない -> 何度実行しても同じ結果が得られる
+echo "======= default ======="
 echo rand(100)
 echo rand(5.5)
 echo rand(20..25)
@@ -27,8 +28,35 @@ echo rand(int8)
 echo rand(uint16)
 echo rand(range[10..20])
 
-## random seed
+## シード値の設定
+echo "======= set seed ======="
 var init = initRand(5)
 echo init.rand(100)
+echo init.next() #uint64が返る
 
+## 乱数期の初期化
+echo "======= randomized ======="
+randomize()
+echo rand(100)
+echo rand(5.5)
+echo rand(20..25)
+echo rand(1.5..2.5)
+echo rand(int8)
+echo rand(uint16)
+echo rand(range[10..20])
 
+## ランダムに抽出
+const
+  mySet = {1, 3, 5, 7, 9}
+  myArray = [0, 2, 4, 6, 8]
+echo "======= sample ======="
+echo mySet.sample #randomize後なので変化する
+echo init.sample(myArray) #seedで固定してるので変化しない
+
+## シャッフル
+var myArray2 = [10, 20, 30, 40, 50]
+echo "======= shuffle ======="
+init.shuffle(myArray2)
+echo myArray2
+shuffle(myArray2)
+echo myArray2
