354436a9b65d6d9cbca914c6fd21c33a7fcd9da6
RiRyCasals <rirycasals@gmail.com>
2022-02-18 13:56:46 +0900

    お試しファイル操作

---
+++ src/exercise/trial/trialFiles.nim
@@ -0,0 +1,10 @@
+import os
+
+let filePath = "trial/testFile.txt"
+
+if fileExists(filePath):
+  for line in lines(filePath):
+    echo line
+else:
+  echo filePath, " is not found."
+  echo "Try execute in the exercise directory."
