6c644def620865f1ebb31e41609871c1e2371f60
RiRyCasals <rirycasals@gmail.com>
2022-03-09 23:31:16 +0900

    オブジェクトのサイズ確認

--- src/exercise/trial/trialByteSize.nim
+++ src/exercise/trial/trialByteSize.nim
@@ -1,3 +1,4 @@
+#==== underflow, overflow ====
 let a: int = 0
 echo sizeof(a)
 
@@ -6,3 +7,28 @@ echo sizeof(b)
 
 dec(b)
 echo b
+
+inc(b)
+echo b
+
+
+#==== size of object type ====
+
+type
+  Thing = object
+    a: uint32
+    b: uint8
+    c: uint16
+
+var t: Thing
+echo "-- size --"
+echo "size of t   : ", sizeof(t)
+echo "size of t.a : ", sizeof(t.a)
+echo "size of t.b : ", sizeof(t.b)
+echo "size of t.c : ", sizeof(t.c)
+
+echo "-- addr --"
+echo "addr of t   : ", repr(t.addr)
+echo "addr of t.a : ", repr(t.a.addr)
+echo "addr of t.b : ", repr(t.b.addr)
+echo "addr of t.c : ", repr(t.c.addr)
