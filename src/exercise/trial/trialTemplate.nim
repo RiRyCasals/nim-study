94c08eae610b4da3e2ec84612e0df371a0095ed5
RiRyCasals <rirycasals@gmail.com>
2022-04-16 21:29:00 +0900

    templateの使い方を追加

--- src/exercise/trial/trialTemplate.nim
+++ src/exercise/trial/trialTemplate.nim
@@ -7,7 +7,6 @@
   template呼び出し時に，呼び出し式の最後に : と続けてブロック文を記述するとtemplate引数の最後にブロック文を渡せる
 ]#
 
-# どういうときに使えばいいんだろう？？？
 
 template echoBlock(blockName, x: untyped): untyped =
   echo "===== ", astToStr(blockName), " ====="
@@ -19,3 +18,12 @@ echoBlock(test):
   var x: int
   x = 10
   echo x
+
+template htmlTag(tag): untyped =
+  proc tag(): string = "<" & astToStr(tag) & ">"
+
+htmlTag(br) # br() が作成される
+echo br()
+
+htmlTag(li) # li() が作成される
+echo li()
