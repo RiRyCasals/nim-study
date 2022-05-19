15d93a99d377a0287b715e29be0e3782461f0425
RiRyCasals <rirycasals@gmail.com>
2022-05-19 10:29:31 +0900

    int操作から文字列操作に

--- src/atcoder/src/abc081a.nim
+++ src/atcoder/src/abc081a.nim
@@ -1,10 +1,4 @@
 import strutils
 
-var a: int = stdin.readLine.parseInt
-
-proc counter(n: var int): int =
-  while n > 0:
-    result += n mod 10
-    n = n.div(10)
-
-echo counter(a)
+let s: string = stdin.readLine
+echo s.count("1")
