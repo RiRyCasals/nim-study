9b85f58c2f17614b57c4c9a0e3b85d6c4b80360d
RiRyCasals <rirycasals@gmail.com>
2022-05-26 11:59:41 +0900

    リファクタリング

--- src/atcoder/src/abc049c.nim
+++ src/atcoder/src/abc049c.nim
@@ -4,19 +4,14 @@ let s = stdin.readLine
 
 var dreamN = s.count("dream");
 var dreamerN = s.count("dreamer");
-var eraseN= s.count("erase");
-var eraserN= s.count("eraser");
-var dreameraserN= s.count("dreamerase");
+var eraseN = s.count("erase");
+var eraserN = s.count("eraser");
+var dreameraseN = s.count("dreamerase");
 
+dreamN -= dreamerN
+eraseN -= eraserN
 
-if dreamN >= dreamerN:
-  dreamN -= dreamerN
-  dreamerN *= -1
-if eraseN >= eraserN:
-  eraseN -= eraserN
-  eraserN *= -1
-
-if (5*dreamN - 7*dreamerN + 5*eraseN - 6*eraserN - 2*dreameraserN) == s.len:
+if (5*dreamN + 7*dreamerN + 5*eraseN + 6*eraserN - 2*dreameraseN) == s.len:
   echo "YES"
 else:
   echo "NO"
